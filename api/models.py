be0a738674f0e2662b7b90681d1b99b18a489d39
Scott Larribeau <slarribeau@gmail.com>
2016-05-12 13:19:53 -0500

    add proper sql query

--- api/models.py
+++ api/models.py
@@ -18,7 +18,8 @@ s
 from api.models import StopTimes
 
 Below query returns routes serviced by a stop and the next departure time.
-select route_short_name, api_trips.trip_headsign, MIN(departure_time) from api_stoptimes inner join api_trips on api_stoptimes.trip_id = api_trips.trip_id inner join api_routes on api_trips.route_id=api_routes.route_id inner join api_stops on api_stoptimes.stop_id = api_stops.stop_id where departure_time > '17:18:00' AND api_trips.service_id = '20160221_10' and api_stops.stop_id ='4' GROUP BY route_short_name, api_trips.trip_headsign;
+select route_short_name, api_trips.trip_headsign, MIN(departure_time) from api_stoptimes inner join api_trips on api_stoptimes.trip_id = api_trips.id inner join api_routes on api_trips.route_id=api_routes.id inner join api_stops on api_stoptimes.stop_id = api_stops.id where departure_time > '17:18:00' AND api_trips.service_id = '20160221_10' and api_stops.stop_id ='4' GROUP BY route_short_name, api_trips.trip_headsign;
+
 
 
 Below is 'training wheel' query:
