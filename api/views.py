fa391b5f945296c12e251dff546bff4420afa05b
Scott Larribeau <slarribeau@gmail.com>
2016-05-13 02:13:46 -0500

    Accept route as parameter to REST URL.

--- api/views.py
+++ api/views.py
@@ -25,7 +25,7 @@ def stops_list(request):
 def departures_detail(request, pk):
     print("departure detail" + pk);
     time="'17:18:00'"
-    stop = "'4'"
+    stop = "'"+pk+"'"
 
     departures=Routes.objects.raw("select api_routes.id, route_short_name, api_trips.trip_headsign, MIN(departure_time) from api_stoptimes inner join api_trips on api_stoptimes.trip_id = api_trips.id inner join api_routes on api_trips.route_id=api_routes.id inner join api_stops on api_stoptimes.stop_id = api_stops.id where departure_time > " + time + " AND api_trips.service_id = '20160221_10' and api_stops.stop_id = " + stop + " GROUP BY api_routes.id, route_short_name, api_trips.trip_headsign;")
 
